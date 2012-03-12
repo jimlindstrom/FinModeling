961ee267f3fa3d40eb33e443106f007f62130cb2
Jim Lindstrom <jim.lindstrom@gmail.com>
2012-03-12 19:43:06 -0400

    Added caching to liabs_and_equity.summary and net_income.summary, which dropped runs on the 2nd-nth time from 2min to 18sec.

--- lib/liabs_and_equity_calculation.rb
+++ lib/liabs_and_equity_calculation.rb
@@ -12,12 +12,39 @@ module FinModeling
       pretty_period = period.to_pretty_s
       if @summary[pretty_period].nil?
         @summary[pretty_period] = super(period, type_to_flip="debit", flip_total=true)
-        classify_all_rows(@summary[pretty_period].rows, lookahead=[4, @summary[pretty_period].rows.length-1].min)
+        if !lookup_cached_classifications(@summary[pretty_period].rows)
+          classify_all_rows(@summary[pretty_period].rows, lookahead=[4, @summary[pretty_period].rows.length-1].min)
+          save_cached_classifications(@summary[pretty_period].rows)
+        end
       end
       return @summary[pretty_period]
     end
 
     private
+
+    BASE_FILENAME = "summaries/liabs_and_equity_"
+    def lookup_cached_classifications(rows)
+      key = Digest::SHA1.hexdigest(rows.map{ |row| row[:key] }.join)
+      filename = BASE_FILENAME + key + ".txt"
+      return false if !File.exists?(filename)
+
+      f = File.open(filename, "r")
+      rows.each do |row|
+        row[:type] = f.gets.chomp.to_sym
+      end
+      f.close
+      return true
+    end
+
+    def save_cached_classifications(rows)
+      key = Digest::SHA1.hexdigest(rows.map{ |row| row[:key] }.join)
+      filename = BASE_FILENAME + key + ".txt"
+      f = File.open(filename, "w")
+      rows.each do |row|
+        f.puts row[:type].to_s
+      end
+      f.close
+    end
 
     # simple viterbi classifier, with 2-element lookahead
     def classify_all_rows(rows, lookahead)
