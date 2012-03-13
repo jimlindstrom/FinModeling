d3cc828a2d65bc3710cbc2aeeb89c25020b76372
Jim Lindstrom <jim.lindstrom@gmail.com>
2012-03-13 07:39:15 -0400

    Added caching of SecQuery::Entity's, so we we don't have to do stock ticker lookups every time. Saves 5-10sec.

--- lib/company.rb
+++ lib/company.rb
@@ -1,18 +1,55 @@
+module SecQuery
+  class Filing
+    def write_constructor(file, item)
+      file.puts "filing = { :cik => \"#{@cik}\", :title => \"#{@title}\", :summary => \"#{@summary}\", " + 
+                ":link => \"#{@link.gsub(/"/, "\\\"")}\", :term => \"#{@term}\", :date => \"#{@date}\", :file_id => \"#{@file_id}\" }"
+      file.puts "#{item} = SecQuery::Filing.new(filing)"
+    end
+  end
+  class Entity
+    def write_constructor(filename)
+      file = File.open(filename, "w")
+      filing_names = []
+      @filings.select{ |x| x.title =~ /^10-/ }.each_with_index do |filing, index|
+        filing_name = "item_#{index}"
+        filing.write_constructor(file, filing_name)
+        filing_names.push filing_name
+      end
+      file.puts "@entity = SecQuery::Entity.new({ :name => \"#{@name.gsub(/"/, "\\\"")}\", :filings => [#{filing_names.join(',')}] })"
+      file.close
+    end
+
+    def self.load(filename)
+      return nil if !File.exists?(filename)
+      eval(File.read(filename))
+      return @entity
+    end
+  end
+end
+
 module FinModeling
   class Company
     def initialize(entity)
       @entity = entity
     end
   
+    BASE_FILENAME = "companies/"
     def self.find(stock_symbol)
+      filename = BASE_FILENAME + stock_symbol.upcase + ".rb"
+      entity = SecQuery::Entity.load(filename)
+      return Company.new(entity) if !entity.nil?
       begin
         entity = SecQuery::Entity.find(stock_symbol, { :relationships => false, 
                                                        :transactions  => false, 
                                                        :filings       => true })
                                                        #:filings       => {:start=> 0, :count=>20, :limit=> 20} })
         return nil if !entity
+        entity.write_constructor(filename)
         return Company.new(entity)
-      rescue
+      rescue Exception => e
+        puts "Warning: failed to load entity"
+        puts "\t" + e.message
+        puts "\t" + e.backtrace.inspect.gsub(/, /, "\n\t ")
         return nil
       end
     end
