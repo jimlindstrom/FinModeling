ce193604f0bad3cfee0e8b235e8be493bf7eea12
Jim Lindstrom <jim.lindstrom@gmail.com>
2012-03-13 20:00:58 -0400

    DRYed up the *_calculation's by moving common code into shared modules.

--- lib/assets_calculation.rb
+++ lib/assets_calculation.rb
@@ -1,30 +1,10 @@
 module FinModeling
-  class AssetsCalculation < CompanyFilingCalculation
 
-    ALL_STATES  =             [ :oa, :fa ]
-    ALLOWED_STATES = { nil => [ :oa, :fa ],
-                       :oa => [ :oa, :fa ],
-                       :fa => [ :oa, :fa ] }
+  module CanCacheClassifications
+    protected
 
-    def summary(period)
-      @summary = { } if @summary.nil?
-      pretty_period = period.to_pretty_s
-      if @summary[pretty_period].nil?
-        @summary[pretty_period] = super(period, type_to_flip="credit", flip_total=false)
-        if !lookup_cached_classifications(@summary[pretty_period].rows)
-          classify_all_rows(@summary[pretty_period].rows, lookahead=[4, @summary[pretty_period].rows.length-1].min)
-          save_cached_classifications(@summary[pretty_period].rows)
-        end
-      end
-      return @summary[pretty_period]
-    end
-
-    private
-
-    BASE_FILENAME = "summaries/ai_"
-    def lookup_cached_classifications(rows)
-      key = Digest::SHA1.hexdigest(rows.map{ |row| row[:key] }.join)
-      filename = BASE_FILENAME + key + ".txt"
+    def lookup_cached_classifications(base_filename, rows)
+      filename = rows_to_filename(base_filename, rows)
       return false if !File.exists?(filename)
 
       f = File.open(filename, "r")
@@ -35,9 +15,8 @@ module FinModeling
       return true
     end
 
-    def save_cached_classifications(rows)
-      key = Digest::SHA1.hexdigest(rows.map{ |row| row[:key] }.join)
-      filename = BASE_FILENAME + key + ".txt"
+    def save_cached_classifications(base_filename, rows)
+      filename = rows_to_filename(base_filename, rows)
       f = File.open(filename, "w")
       rows.each do |row|
         f.puts row[:type].to_s
@@ -45,21 +24,44 @@ module FinModeling
       f.close
     end
 
-    # simple viterbi classifier, with 2-element lookahead
-    def classify_all_rows(rows, lookahead)
+    private
+
+    def rows_to_filename(base_filename, rows)
+      key = Digest::SHA1.hexdigest(rows.map{ |row| row[:key] }.join)
+      filename = base_filename + key + ".txt"
+    end
+  end
+
+  module CanCacheSummaries
+    protected
+
+    def lookup_cached_summary(key)
+      @summary_cache = { } if @summary_cache.nil?
+      return @summary_cache[key]
+    end
+
+    def save_cached_summary(key, summary)
+      @summary_cache[key] = summary
+    end
+  end
+
+  module CanClassifyRows # simple viterbi classifier, with N-element lookahead
+    protected
+
+    def classify_all_rows(all_states, next_states, rows, row_item_type, lookahead)
       prev_state = nil
       rows.each_with_index do |row, idx|
         lookahead = [lookahead, rows.length-idx-lookahead].min
-        row[:type] = classify_row(rows, idx, prev_state, lookahead)[:state]
+        row[:type] = classify_row(all_states, next_states, rows, row_item_type, idx, prev_state, lookahead)[:state]
         raise RuntimeError.new("couldn't classify....") if row[:type].nil?
 
         prev_state = row[:type]
       end
     end
 
-    def classify_row(rows, idx, prev_state, lookahead)
-      ai = FinModeling::AssetsItem.new(rows[idx][:key])
-      estimates = ai.classification_estimates
+    def classify_row(all_states, next_states, rows, row_item_type, idx, prev_state, lookahead)
+      item = row_item_type.new(rows[idx][:key])
+      estimates = item.classification_estimates
 
       best_est           = -10000
       best_state         = nil
@@ -67,8 +69,8 @@ module FinModeling
       best_allowed_est   = -10000
       best_allowed_state = nil
 
-      ALL_STATES.each do |state|
-        future_error = (lookahead == 0) ?  0.0 : classify_row(rows, idx+1, state, lookahead-1)[:error]
+      all_states.each do |state|
+        future_error = (lookahead == 0) ?  0.0 : classify_row(all_states, next_states, rows, row_item_type, idx+1, state, lookahead-1)[:error]
         cur_est = estimates[state] - future_error
 
         if cur_est > best_est
@@ -76,7 +78,7 @@ module FinModeling
           best_state = state
         end
 
-        if !ALLOWED_STATES[prev_state].nil? and ALLOWED_STATES[prev_state].include?(state)
+        if !next_states[prev_state].nil? and next_states[prev_state].include?(state)
           if cur_est > best_allowed_est
             best_allowed_est   = cur_est
             best_allowed_state = state
@@ -87,6 +89,37 @@ module FinModeling
 
       return { :state => best_allowed_state,
                :error => best_est - best_allowed_est }
+    end
+
+  end
+
+  class AssetsCalculation < CompanyFilingCalculation
+    include CanCacheClassifications
+    include CanCacheSummaries
+    include CanClassifyRows
+
+    BASE_FILENAME = "summaries/ai_"
+
+    ALL_STATES  =          [ :oa, :fa ]
+    NEXT_STATES = { nil => [ :oa, :fa ],
+                    :oa => [ :oa, :fa ],
+                    :fa => [ :oa, :fa ] }
+
+    def summary(period)
+      summary_cache_key = period.to_pretty_s
+      summary = lookup_cached_summary(summary_cache_key)
+      return summary if !summary.nil?
+
+      summary = super(period, type_to_flip="credit", flip_total=false)
+      if !lookup_cached_classifications(BASE_FILENAME, summary.rows)
+        lookahead = [4, summary.rows.length-1].min
+        classify_all_rows(ALL_STATES, NEXT_STATES, summary.rows, FinModeling::AssetsItem, lookahead)
+        save_cached_classifications(BASE_FILENAME, summary.rows)
+      end
+
+      save_cached_summary(summary_cache_key, summary)
+
+      return summary
     end
 
   end
--- lib/net_income_calculation.rb
+++ lib/net_income_calculation.rb
@@ -1,98 +1,37 @@
 module FinModeling
   class NetIncomeCalculation < CompanyFilingCalculation
-
-    ALL_STATES  =                [ :or, :cogs, :oe, :oibt, :fibt, :tax, :ooiat, :fiat ]
-    ALLOWED_STATES = { nil    => [ :or, :cogs, :oe, :oibt, :fibt, :tax, :ooiat, :fiat ],
-                       :or    => [ :or, :cogs, :oe, :oibt, :fibt, :tax, :ooiat, :fiat ],
-                       :cogs  => [      :cogs, :oe, :oibt, :fibt, :tax, :ooiat, :fiat ],
-                       :oe    => [             :oe, :oibt, :fibt, :tax, :ooiat, :fiat ],
-                       :oibt  => [                  :oibt, :fibt, :tax, :ooiat, :fiat ], # obit/fibt can cycle back/forth
-                       :fibt  => [                  :obit, :fibt, :tax, :ooiat, :fiat ], # obit/fibt can cycle back/forth
-                       :tax   => [                                      :ooiat, :fiat ], # tax can't go to itself. only 1 such item.
-                       :ooiat => [                                      :ooiat, :fiat ], # ooiat/fiat can cycle back/forth
-                       :fiat  => [                                      :ooiat, :fiat ] }# ooiat/fiat can cycle back/forth
-
-    def summary(period)
-      @summary = { } if @summary.nil?
-      pretty_period = period.to_pretty_s
-      if @summary[pretty_period].nil?
-        @summary[pretty_period] = super(period, type_to_flip="debit",  flip_total=true)
-        if !lookup_cached_classifications(@summary[pretty_period].rows)
-          classify_all_rows(@summary[pretty_period].rows, lookahead=[4, @summary[pretty_period].rows.length-1].min)
-          save_cached_classifications(@summary[pretty_period].rows)
-        end
-      end
-      return @summary[pretty_period]
-    end
-
-    private
+    include CanCacheClassifications
+    include CanCacheSummaries
+    include CanClassifyRows
 
     BASE_FILENAME = "summaries/net_income_"
-    def lookup_cached_classifications(rows)
-      key = Digest::SHA1.hexdigest(rows.map{ |row| row[:key] }.join)
-      filename = BASE_FILENAME + key + ".txt"
-      return false if !File.exists?(filename)
 
-      f = File.open(filename, "r")
-      rows.each do |row|
-        row[:type] = f.gets.chomp.to_sym
-      end
-      f.close
-      return true
-    end
+    ALL_STATES  =             [ :or, :cogs, :oe, :oibt, :fibt, :tax, :ooiat, :fiat ]
+    NEXT_STATES = { nil    => [ :or, :cogs, :oe, :oibt, :fibt, :tax, :ooiat, :fiat ],
+                    :or    => [ :or, :cogs, :oe, :oibt, :fibt, :tax, :ooiat, :fiat ],
+                    :cogs  => [      :cogs, :oe, :oibt, :fibt, :tax, :ooiat, :fiat ],
+                    :oe    => [             :oe, :oibt, :fibt, :tax, :ooiat, :fiat ],
+                    :oibt  => [                  :oibt, :fibt, :tax, :ooiat, :fiat ], # obit/fibt can cycle back/forth
+                    :fibt  => [                  :obit, :fibt, :tax, :ooiat, :fiat ], # obit/fibt can cycle back/forth
+                    :tax   => [                                      :ooiat, :fiat ], # tax can't go to itself. only 1 such item.
+                    :ooiat => [                                      :ooiat, :fiat ], # ooiat/fiat can cycle back/forth
+                    :fiat  => [                                      :ooiat, :fiat ] }# ooiat/fiat can cycle back/forth
 
-    def save_cached_classifications(rows)
-      key = Digest::SHA1.hexdigest(rows.map{ |row| row[:key] }.join)
-      filename = BASE_FILENAME + key + ".txt"
-      f = File.open(filename, "w")
-      rows.each do |row|
-        f.puts row[:type].to_s
-      end
-      f.close
-    end
-
-    # simple viterbi classifier, with N-element lookahead
-    def classify_all_rows(rows, lookahead)
-      prev_state = nil
-      rows.each_with_index do |row, idx|
-        lookahead = [lookahead, rows.length-idx-lookahead].min
-        row[:type] = classify_row(rows, idx, prev_state, lookahead)[:state]
-        raise RuntimeError.new("couldn't classify....") if row[:type].nil?
-
-        prev_state = row[:type]
-      end
-    end
-
-    def classify_row(rows, idx, prev_state, lookahead)
-      isi = FinModeling::IncomeStatementItem.new(rows[idx][:key])
-      estimates = isi.classification_estimates
-
-      best_est           = -10000
-      best_state         = nil
-
-      best_allowed_est   = -10000
-      best_allowed_state = nil
-
-      ALL_STATES.each do |state|
-        future_error = (lookahead == 0) ?  0.0 : classify_row(rows, idx+1, state, lookahead-1)[:error]
-        cur_est = estimates[state] - future_error
-
-        if cur_est > best_est
-          best_est   = cur_est
-          best_state = state
-        end
-
-        if !ALLOWED_STATES[prev_state].nil? and ALLOWED_STATES[prev_state].include?(state)
-          if cur_est > best_allowed_est
-            best_allowed_est   = cur_est
-            best_allowed_state = state
-          end
-        end
-
+    def summary(period)
+      summary_cache_key = period.to_pretty_s
+      summary = lookup_cached_summary(summary_cache_key)
+      return summary if !summary.nil?
+    
+      summary = super(period, type_to_flip="debit", flip_total=true)
+      if !lookup_cached_classifications(BASE_FILENAME, summary.rows)
+        lookahead = [4, summary.rows.length-1].min
+        classify_all_rows(ALL_STATES, NEXT_STATES, summary.rows, FinModeling::IncomeStatementItem, lookahead)
+        save_cached_classifications(BASE_FILENAME, summary.rows)
       end
 
-      return { :state => best_allowed_state,
-               :error => best_est - best_allowed_est }
+      save_cached_summary(summary_cache_key, summary)
+
+      return summary
     end
  
   end
