699be03aba31487f4b5c66288df9062907436302
Jim Lindstrom <jim.lindstrom@gmail.com>
2012-03-21 19:16:17 -0400

    Showing Operating PM in the income statement analysis now.

--- lib/income_statement_analyses.rb
+++ lib/income_statement_analyses.rb
@@ -2,6 +2,13 @@ module FinModeling
 
   module IncomeStatementAnalyses
     def print_extras
+      lr = operating_pm_row.valid_vals.linear_regression
+      puts "\t\toperating pm: "+
+           "a:#{lr.a.to_s.cap_decimals(4)}, "+
+           "b:#{lr.b.to_s.cap_decimals(4)}, "+
+           "r:#{lr.r.to_s.cap_decimals(4)}, "+
+           "var:#{operating_pm_row.valid_vals.variance.to_s.cap_decimals(4)}"
+
       lr = asset_turnover_row.valid_vals.linear_regression
       puts "\t\tasset turnover: "+
            "a:#{lr.a.to_s.cap_decimals(4)}, "+
@@ -9,13 +16,16 @@ module FinModeling
            "r:#{lr.r.to_s.cap_decimals(4)}, "+
            "var:#{asset_turnover_row.valid_vals.variance.to_s.cap_decimals(4)}"
 
-
       lr = revenue_growth_row.valid_vals.linear_regression
       puts "\t\trevenue growth: "+
            "a:#{lr.a.to_s.cap_decimals(4)}, "+
            "b:#{lr.b.to_s.cap_decimals(4)}, "+
            "r:#{lr.r.to_s.cap_decimals(4)}, "+
            "var:#{revenue_growth_row.valid_vals.variance.to_s.cap_decimals(4)}"
+    end
+
+    def operating_pm_row
+      find_row_by_key('Operating PM')
     end
 
     def asset_turnover_row
