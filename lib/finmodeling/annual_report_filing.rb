5a9f170754c7f0db8d9e1827c7966efbdd704bf0
Jim Lindstrom <jim.lindstrom@gmail.com>
2013-03-09 21:06:38 -0500

    Showing which statements in a filing are invlaid

--- lib/finmodeling/annual_report_filing.rb
+++ lib/finmodeling/annual_report_filing.rb
@@ -103,6 +103,9 @@ module FinModeling
     end
 
     def is_valid?
+      puts "income statment is not valid" if !income_statement.is_valid?
+      puts "balance sheet is not valid" if !balance_sheet.is_valid?
+      puts "cash flow statement is not valid" if !cash_flow_statement.is_valid?
       return (income_statement.is_valid? and balance_sheet.is_valid? and cash_flow_statement.is_valid?)
     end
 
